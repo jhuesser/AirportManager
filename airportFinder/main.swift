b0559d0d2bcd7520f1d1ca7f6ba03c57b518b0d1
Jonas Hüsser <jonas.huesser+github@gmail.com>
2016-09-20 21:55:48 +0200

    Initial Commit

---
+++ airportFinder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  airportFinder
+//
+//  Created by Jonas Hüsser on 20.09.16.
+//  Copyright © 2016 Jonas Hüsser. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
